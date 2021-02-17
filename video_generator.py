9d4885731ceb62e726290a2727bfc2c0319c4c6f
Sabathier Remy <remy.sabathier@supelec.fr>
2021-02-17 14:05:37 +0100

    reshape video generator

--- video_generator.py
+++ video_generator.py
@@ -6,24 +6,50 @@ import imageio
 import time
 from skimage.util import img_as_ubyte
 from skimage import data
+from skimage.transform import EuclideanTransform, rotate
+
+# Class that creates random frames and outputs list of jitters (x,y,r)
 
 
-class Env():
+class Jitters():
+    '''The Jitter class creates random movement to apply to frames'''
 
-    def __init__(self, n_shapes=1, size=512, frames=100, min_speed=1, max_speed=10, possible_radius=[10], min_jitters=-15, max_jitters=15, max_step=2):
-        # Blank image
+    def __init__(self, size=1000, min_jitters=-30, max_jitters=30, max_step=2, min_rotation=-10, max_rotation=10, max_step_rotation=2):
 
-        noisy_image = img_as_ubyte(data.camera())
+        self.size = size
+        self.min_jitters = min_jitters
+        self.max_jitters = max_jitters
+        self.max_step = max_step
+        self.min_rotation = min_rotation
+        self.max_rotation = max_rotation
+        self.max_step_rotation = max_step_rotation
 
-        self.map = np.zeros((frames, size, size), dtype=np.uint8)
+    def compute_jitters(self):
+        '''Compute the jitter and output a dictionnary with all the jitter parameters'''
+        self.jitter_x = np.clip(np.cumsum(np.random.randint(
+            self.max_step*2+1, size=self.size) - 2), a_min=self.min_jitters, a_max=self.max_jitters)
 
-        for i in range(frames):
-            self.map[i] = noisy_image
+        self.jitter_y = np.clip(np.cumsum(np.random.randint(
+            self.max_step*2+1, size=self.size) - 2), a_min=self.min_jitters, a_max=self.max_jitters)
 
-        self.crop_map = np.zeros(
-            (frames, int(size/2), int(size/2)), dtype=np.uint8)
-        self.jitter_map = np.zeros(
-            (frames, int(size/2), int(size/2)), dtype=np.uint8)
+        self.jitter_r = np.clip(np.cumsum(np.random.randint(
+            self.max_step_rotation*2+1, size=self.size) - 2), a_min=self.min_rotation, a_max=self.max_rotation)
+
+        return {'X': self.jitter_x, 'Y': self.jitter_y, 'R': self.jitter_r}
+
+
+# Class that creates frames
+class FrameBuilder():
+    '''Class that creates frames with a background and some images'''
+
+    def __init__(self,
+                 n_shapes=1,
+                 size=512,
+                 frames=100,
+                 min_speed=1,
+                 max_speed=10,
+                 possible_radius=[10]
+                 ):
 
         self.size = size
         self.n_shapes = n_shapes
@@ -36,20 +62,22 @@ class Env():
         # Radius parameter
         self.possible_radius = possible_radius
 
-        # Jitters parameters
-        self.min_jitters = min_jitters
-        self.max_jitters = max_jitters
-        self.max_step = max_step
-
         # Shape dictionnary
         self.shape_dict = {}
 
-        self.jitter_x = []
-        self.jitter_y = []
+        # Array that will store all the frames
+        self.map = np.zeros((frames, size, size), dtype=np.uint8)
 
-        self.shapes = []
+        # The background image
+        noisy_image = img_as_ubyte(data.camera())
+        # White frames
+        self.map = np.zeros((frames, size, size), dtype=np.uint8)
+
+        for i in range(frames):
+            self.map[i] = noisy_image
 
     # Create the dictionnary with all shape parameters
+
     def _create_shape(self):
 
         a = np.random.uniform(2, self.size-2)
@@ -123,6 +151,7 @@ class Env():
         return shape_param
 
     def build_frames(self):
+
         # Initialization: build initial shapes
         for i in range(self.n_shapes):
             self.shape_dict[i] = self._create_shape()
@@ -137,7 +166,7 @@ class Env():
                 x = x + t*self.shape_dict[elt]['translation'][0]
                 y = y + t*self.shape_dict[elt]['translation'][1]
 
-                # If center outside the image, remove it
+                # If center outside the image, replace it by a new shape
                 if (x > self.size-1) or (x < 0) or (y > self.size-1) or (y < 0):
                     self.shape_dict[elt] = self._create_shape()
 
@@ -148,26 +177,57 @@ class Env():
                     self.map[f, rr, cc] = self.shape_dict[elt]['color']
                     self.shape_dict[elt]['time_step'] += 1
 
+        return {'frames': self.map}
+
+
+# Class that takes frames and jitters and compute the jittered frames
+class JitterEnv():
+    '''Takes frames and jitters as parameters and computes the jittered frames'''
+
+    def __init__(self, frames, jitters, crop_ratio=1/4):
+
+        self.map = frames['frames']
+        self.frames = self.map.shape[0]
+        self.size = self.map.shape[1]
+
+        self.jitter_x = jitters['X']
+        self.jitter_y = jitters['Y']
+        self.jitter_r = jitters['R']
+
+        self.crop_ratio = crop_ratio
+
+        l1, l2 = int(self.crop_ratio*self.size), int(3 *
+                                                     self.crop_ratio*self.size)
+
+        self.jitter_map = np.zeros((self.frames, l2-l1, l2-l1), dtype=np.uint8)
+        self.crop_map = np.zeros((self.frames, l2-l1, l2-l1), dtype=np.uint8)
+
     def compute_crop_map(self):
-        self.crop_map = self.map[:, self.size //
-                                 4:3*self.size//4, self.size//4:3*self.size//4]
+
+        l1, l2 = int(self.crop_ratio*self.size), int(3 *
+                                                     self.crop_ratio*self.size)
+        self.crop_map = self.map[:, l1:l2, l1:l2]
 
     def compute_jittered_map(self):
 
-        self.jitter_x = np.clip(np.cumsum(np.random.randint(
-            self.max_step*2+1, size=self.frames) - 2), a_min=self.min_jitters, a_max=self.max_jitters)
-        self.jitter_y = np.clip(np.cumsum(np.random.randint(
-            self.max_step*2+1, size=self.frames) - 2), a_min=self.min_jitters, a_max=self.max_jitters)
-
         for f in range(self.frames):
 
-            j_x, j_y = self.jitter_x[f], self.jitter_y[f]
+            j_x, j_y, j_r = self.jitter_x[f], self.jitter_y[f], self.jitter_r[f]
 
-            self.jitter_map[f] = self.map[f, j_x+self.size //
-                                          4: j_x+3*self.size//4, j_y+self.size//4:j_y+3*self.size//4]
+            # Rotation
+            current_map = rotate(self.map[f], angle=j_r)*255.
+            current_map = current_map.astype(np.uint8)
 
-    def get_map(self):
-        return self.map
+            # Translation
+            l1, l2 = int(self.crop_ratio*self.size), int(3 *
+                                                         self.crop_ratio*self.size)
+            self.jitter_map[f] = current_map[j_x+l1:j_x+l2, j_y+l1:j_y+l2]
+
+            pass
+
+    def compute_maps(self):
+        self.compute_jittered_map()
+        self.compute_crop_map()
 
     def make_gif(self, path, fps=24):
 
@@ -193,36 +253,19 @@ class Env():
 
 if __name__ == '__main__':
 
-    start = time.time()
+    # Compute the jitters
+    jt = Jitters()
+    jitters = jt.compute_jitters()
 
-    # Build the environment
-    env = Env(
-        n_shapes=20,
-        size=512,
-        frames=1000,
-        min_speed=0.1,
-        max_speed=1,
-        possible_radius=[10, 5, 15],
+    # Build random frames
+    rf = FrameBuilder(n_shapes=20, size=512, frames=1000,
+                      min_speed=0.1, max_speed=1, possible_radius=[10, 5, 15])
+    frames = rf.build_frames()
 
-        min_jitters=-30,
-        max_jitters=30,
-        max_step=2,
-    )
-    env.build_frames()
-    stop = time.time()
-
-    print('Frame generated in {:.4f}s'.format(stop-start))
-
-    map = env.get_map()
-
-    # Compute cropped and jittered map
-    env.compute_crop_map()
-    env.compute_jittered_map()
-
-    # Return gif
-    env.make_gif('test.gif')
-
-    # Save jitter plot
+    # Create jittered frames and plot the result
+    env = JitterEnv(frames, jitters)
+    env.compute_maps()
+    env.make_gif('video_frame.gif')
     env.plot_jitters('jitters.png')
 
     pass
